c72f55419d8c9c586844f8d3aad961240b345967
mdmah <mdmahmud3311@gmail.com>
2022-09-06 15:49:19 +0600

    math and more math

--- Operation in Arithmetic/number.py
+++ Operation in Arithmetic/number.py
@@ -1,13 +1,18 @@
-natural_number =
+natural_number = (1,2,3,4,5,6,7,8,9)
+print(natural_number)
 
-positive_number = 1, 2, 3, 4
+positive_number = (1, 2, 3, 4)
+print(positive_number)
 
-negative_number =
+negative_number = (-2,-3,-4)
+print(negative_number)
 
-integer =
+integer = (1,2,3, -5, -8, -9)
+print(integer)
 
-direct_number =
-
-line_number =
-
-rounding =
+direct_number = (-7, 6, +3)
+print(direct_number)
+line_number = (-5, -4, -2, -1, 0, 1, 2, 3, 4, 5)
+print(line_number)
+rounding = (65)
+print(rounding)
