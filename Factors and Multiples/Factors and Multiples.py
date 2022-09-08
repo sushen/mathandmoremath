0b80817e23ef8bb486431c45dbc2896016c4be30
mdmah <mdmahmud3311@gmail.com>
2022-09-08 07:46:18 +0600

    math and more math

---
+++ Factors and Multiples/Factors and Multiples.py
@@ -0,0 +1,28 @@
+add_number =
+
+numbers_power =
+
+square_numer =
+
+cube_number =
+
+number_square_rout =
+
+gunfal_square_rout =
+
+number_cube_rout =
+
+factor =
+
+prime_number =
+
+prime_factor =
+
+negative_factor=
+
+multiples =
+
+lowest_common_multiple =
+
+highst_common_multiple =
+
