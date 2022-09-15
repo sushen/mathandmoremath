308e79d710ec4bb376b39bcb49647e9122b26106
Sushen Biswas <sushenbiswasaga@gmail.com>
2022-09-15 09:52:39 +0600

    Update: All

--- 2. Factors and Multiples/LCM of 3 or 4.py
+++ 2. Factors and Multiples/LCM of 3 or 4.py
@@ -1,23 +1,20 @@
-# TODO 1: we need two Numbers, example 8,9
-# todo : cake higher number = number one> number two
-# todo: the higher number is one
-# todo : or higher number is two
-# todo : higher number reminds number one result = 0
-# todo : higher number reminds number two result = 0
-# todo lcm = higher number
-# todo : higher number = number one * number two
+# TODO 1: we need two Numbers, example 3,4
+number_one = 3
+number_two = 4
 
-
-number_one = int(input("Enter any number:"))
-number_two = int(input("Enter any number:"))
-if number_one >number_two:
+# todo : check higher number = number one> number two
+if number_one > number_two:
     higher_number = number_one
 else:
     higher_number = number_two
+
+# TODO : ?
 while True:
-    if ((higher_number % number_one == 0) and (higher_number % number_two == 0)):
+    print("looping")
+    if (higher_number % number_one == 0) and (higher_number % number_two == 0):
         lcm = higher_number
-        break
-    higher_number = number_one*number_two
-print(lcm)
+    # TODO : When we multiply tow number we got the LCM
+    higher_number = number_one * number_two
+    print(higher_number)
+    break
 
