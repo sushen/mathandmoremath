3c90d257032cf2fe996561975dd739a387b01f4c
Sushen Biswas <sushenbiswasaga@gmail.com>
2022-09-16 09:59:31 +0600

    add : doubledevision.py , Update: LCM of 3 or 4 .py

--- 2. Factors and Multiples/LCM of 3 or 4.py
+++ 2. Factors and Multiples/LCM of 3 or 4.py
@@ -4,13 +4,13 @@ number_three = 12
 number_four = 21
 
 print(number_one)
-print(2**2)
+# print(2**2)
 print(number_two)
-print(5*2)
+# print(5*2)
 print(number_three)
-print(3* 2**2)
+# print(3* 2**2)
 print(number_four)
-print(3*5)
+# print(3*5)
 
 print(f"LCM = {(2**2) * 5 * 3 * 5}")
 
