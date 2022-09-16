592adb594873e02e5c27dbf39ccb1bbeff33621c
Sushen Biswas <sushenbiswasaga@gmail.com>
2022-09-16 09:57:58 +0600

    add : doubledevision.py , Update: LCM of 3 or 4 .py

--- 2. Factors and Multiples/LCM of 3 or 4.py
+++ 2. Factors and Multiples/LCM of 3 or 4.py
@@ -1,22 +1,16 @@
-# TODO 1: we need two Numbers, example 3,4
 number_one = 4
-number_two = 4
+number_two = 6
+number_three = 12
+number_four = 21
 
-# todo : check higher number = number one> number two
-if number_one > number_two :
-    higher_number = number_one
-else :
-    higher_number = number_two
+print(number_one)
+print(2**2)
+print(number_two)
+print(5*2)
+print(number_three)
+print(3* 2**2)
+print(number_four)
+print(3*5)
 
-# TODO : ? বড় সংখ্যাটিকে ছোটটি দিয়ে ভাগ করলে যদি ভাগশেষ 0 হয় তাহলে বড় সংখাটি ল সা গু..
-# todo: তাই বড় সংখাটিকে প্রথম এবং দ্বিতিয় নাম্বারটি দিয়ে ভাগ করি যদি ভাগশেষ 0 না হয় তবে দুটি সংখাকে গুন করে দেই।
-while True :
-    print("looping")
-    if (higher_number % number_one == 0) and (higher_number % number_two == 0) :
-        lcm = higher_number
-    print(lcm)
-    break
-    # TODO : When we multiply tow number we got the LCM
-    higher_number = number_one * number_two
-    print(higher_number)
-    break
+print(f"LCM = {(2**2) * 5 * 3 * 5}")
+
