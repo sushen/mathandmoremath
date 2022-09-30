4686d90efb4a6f0ea8ab265db3abe80e62c6696d
Sushen Biswas <sushenbiswasaga@gmail.com>
2022-09-30 09:11:40 +0600

    Update : operation.py "Use of Docstring -- Docstring cannot catch by python "

--- 1. Operation in Arithmetic/operation.py
+++ 1. Operation in Arithmetic/operation.py
@@ -1,19 +1,34 @@
+"""
+Example 1 : Using Negative number we can define number
+"""
 number_line = -1, -2, -3, -4, -5, 0, 1, 2, 3, 4, 5
 # print(number_line)
-# when i am in canada. the weather in there is
+
+"""
+Example 2 : Using Negative number we can define weather
+"""
+# when I am in canada. The weather in there is
 normal_day = 36
 summer_day = 52
 cold_day = -25
 print(normal_day)
-# Accounts Profit and Loss in business
+
+"""
+Example 3 : Using Negative number we can define Accounts Profit and Loss in business
+"""
+
 day_1_profit = 5000
 day_2_profit = 7000
 day_3_loss = -3000
 day_4_loss = -4000
 day_5_profit = 5000
-line_of_profit_and_loss = 5000, 5000, 7000,-3000, -4000,
-# print(line_of_profit_and_loss)
-# temperature of ice
-tempreture = -25
+line_of_profit_and_loss = 5000, 5000, 7000, -3000, -4000
+print(line_of_profit_and_loss)
+
+"""
+Example 4 : Using Negative number we can define temperature of ice
+"""
+
+temperature = -25
 # Power of your glasses
 power = -90
