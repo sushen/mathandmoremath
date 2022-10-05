d5e447ff1a0816b8a449cc5c333a3e163797700d
Tanjil <tanjilhossainkabbo@gmail.com>
2022-10-05 11:59:37 +0600

    Update: concatenate.py "we will write a function where we will concatenate the multiple fraction."

--- 3. Fractions/concatenate.py
+++ 3. Fractions/concatenate.py
@@ -1,10 +1,20 @@
-"""
-we will write a function where we will concatenate the multiple fraction.
-"""
-fraction_one = 9 / 90
-fraction_two = 110 / 8
-result = fraction_one + fraction_two
-print(result)
+# """
+# we will write a function where we will concatenate the multiple fraction.
+# """
+# fraction_one = 9 / 90
+# fraction_two = 110 / 8
+# result = fraction_one + fraction_two
+# print(result)
 
 # TODO : Chapai Nawabganj Mango supply is 70 % of total exported mango and Bangladesh consume 15 % from Chapai Nawabganj
 #  mango production. This years bangladesh consume 100k mango. How many mango they produce This years.
+
+chapai_nawabganj_mango_supply = 70/100
+bangladesh_consume = 15/chapai_nawabganj_mango_supply
+
+# this_year_bangladesh_consume = 100*1000
+
+"""solution"""
+fifteen_percent = 100*1000
+saventy_percent = ((fifteen_percent*70)/15)
+print(saventy_percent)
