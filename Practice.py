2fe3bbae1c02d679633f046cec5bd154ad5ea401
Tanjil <tanjilhossainkabbo@gmail.com>
2022-10-07 09:44:05 +0600

    Update: concatenate.py "we will write a function where we will concatenate the multiple fraction."

---
+++ Practice.py
@@ -0,0 +1,16 @@
+a = 3/10
+ab = 9/1000
+print(a+ab)
+bc = 8/100
+cd = 7/1000
+print(bc + cd)
+de = 358.28
+ef = 0.732
+fg = 15.2
+print(de+ef+fg)
+arr = [78, 90, 67, 35, 56]
+key = 70
+if key in (arr):
+    print("It is in the array")
+else:
+    print("It's not found")
